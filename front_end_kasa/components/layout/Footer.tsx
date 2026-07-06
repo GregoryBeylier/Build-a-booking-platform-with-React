7d3f72fe01e0406d06aa55cabc96df414939c99d
Gregory <samsung261101@gmail.com>
2026-07-06 21:26:14 +0200

    feat: css Footer

--- front_end_kasa/components/layout/Footer.tsx
+++ front_end_kasa/components/layout/Footer.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="bg-white flex justify-between items-center">
-      <div className="relative w-8 h-8">
+    <div className="bg-white flex justify-between items-center h-[70px] py-2 px-[40px] border-t border-t-[#F5F5F5]">
+      <div className="relative w-[46px] h-[53px]">
         <Image src="/Logomobile.svg" alt="Kasa" fill />
       </div>
-      <p className="">
+      <p className="text-xs font-medium text-[#565656]">
         © {new Date().getFullYear()} Kasa. Tous droits réservés
       </p>
     </div>
