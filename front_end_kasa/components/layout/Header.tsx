df96cfd565c65169789eea6fdd798b2d73d86626
Gregory <samsung261101@gmail.com>
2026-07-06 21:00:03 +0200

    style(header): match Figma desktop layout (centered logo, nav spacing,

--- front_end_kasa/components/layout/Header.tsx
+++ front_end_kasa/components/layout/Header.tsx
@@ -5,21 +5,31 @@ import { Heart, MessageSquare } from "lucide-react";
 
 export default function Header() {
   return (
-    <div className="bg-white">
-      <Logo />
-      <div className="md:hidden">
-        <MobileMenu />
-      </div>
-      <div className="hidden md:flex">
-        <Link href="/">Accueil</Link>
-        <Link href="/about">A propos</Link>
-        <Link href="/add-property">Ajouter un logement </Link>
-        <Link href="/messages">
-          <MessageSquare />
-        </Link>
-        <Link href="/favorites">
-          <Heart />
-        </Link>
+    <div className="md:bg-[#FFFBF9]">
+      <div className="md:flex  md:justify-center items-center bg-white mt-10 mb-10 rounded-[10px] max-w-[782px] mx-auto w-full py-2 px-[100px] shadow whitespace-nowrap">
+        <div className="md:hidden">
+          <MobileMenu />
+        </div>
+        <div className="hidden md:flex gap-5 mr-[50px]">
+          <Link href="/">Accueil</Link>
+          <Link href="/about">À propos</Link>
+        </div>
+        <Logo />
+        <div className="hidden md:flex gap-5 ml-[30px]">
+          <Link href="/add-property" className="text-[#99331A]">
+            {" "}
+            + Ajouter un logement{" "}
+          </Link>
+          <div className="flex gap-3 translate-y-[2px] ">
+            <Link href="/favorites">
+              <Heart size={17} className="text-[#99331A]" />
+            </Link>
+            <span className="w-[1px] h-4 bg-[#99331A]" />
+            <Link href="/messages">
+              <MessageSquare size={17} className="text-[#99331A]" />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- front_end_kasa/components/ui/Logo.tsx
+++ front_end_kasa/components/ui/Logo.tsx
@@ -3,21 +3,11 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <div className="flex items-center justify-between bg-white">
-      <div className="relative w-24 h-8">
-        <Image
-          src="/Logodesktop.svg"
-          alt="Kasa"
-          fill
-          className="hidden md:block"
-        />
+      <div className="relative w-24 h-8 hidden md:block">
+        <Image src="/Logodesktop.svg" alt="Kasa" fill />
       </div>
-      <div className="relative w-24 h-8">
-        <Image
-          src="/Logomobile.svg"
-          alt="Kasa"
-          fill
-          className="block md:hidden"
-        />
+      <div className="relative w-24 h-8 block md:hidden">
+        <Image src="/Logomobile.svg" alt="Kasa" fill />
       </div>
     </div>
   );
