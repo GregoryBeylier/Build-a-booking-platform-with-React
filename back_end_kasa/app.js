ad0c086d8460652ff6b59d52e921df5c4cfbcfbb
Gregory <samsung261101@gmail.com>
2026-07-20 16:56:13 +0200

    Allow production frontend origin in CORS config

--- back_end_kasa/app.js
+++ back_end_kasa/app.js
@@ -12,7 +12,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:8000" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8000",
+      "https://build-a-booking-platform-with-react.vercel.app",
+    ],
+  }),
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
